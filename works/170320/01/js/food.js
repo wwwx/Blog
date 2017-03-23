baf786b0ea764440dfab32ed97603022f7f380c3
wwwx <risin_wangx@outlook.com>
2017-03-23 18:35:19 +0800

    update

--- works/170320/01/js/food.js
+++ works/170320/01/js/food.js
@@ -2,20 +2,10 @@
 
 $(function(){
 
-// 全局调用方法methods
-    var m = {
-        // 格式化字符串
-        sprintf: function(){
-            var str = arguments[0] || '';
-            for (var i=1, n=arguments.length; i<n; i++) {
-                str = str.replace(/%s/, arguments[i].toString());
-            }
-            return str;
-        }
-    }
-
 // 
-    var item = '<li><a href="%s">\
+    var food = {
+        // 
+        item: '<li><a href="%s">\
                     <div class="pic"><img src="%s" /><span>322*322</span></div>\
                     <p class="desc">%s</p>\
                     <p class="price clearfix">\
@@ -23,84 +13,80 @@ $(function(){
                         <span class="fr">市场价:<del>￥%s</del></span>\
                     </p>\
                     <p class="buy"><span class="btn-buy">立即购买 > </span></p>\
-                </a></li>';
-
-    var win = $(window),
-        navbar = $('#navbar'),
-        navAnchor = $('#navAnchor'),
-        box = $('.box');
-
-
-// 
-    var food = {
-        init: function(){
+                </a></li>'
+        ,navbar : $('#navbar')
+        ,navAnchor : $('#navAnchor')
+        ,box : $('.box')
+        //
+        ,init: function(){
             var keys = ['event_zcms_mysy', 'event_zcms_xxls', 'event_zcms_sxsg', 'event_zcms_bkzq'];
             for (var i in keys) {
                 this.getList(keys[i]);
             }
 
             this.stick();
-            this.nav(); 
+            this.navTap(); 
             this.navScroll();
-        },
-        getList: function(key){
+        }
+        ,getList: function(key){
+            var that = this;
             var url = "//search.ule.com/api/recommend?jsoncallback=?&restype=2001";
             var data = {moduleKeys: key, times: new Date().getTime()};
             $.ajax({
                 type: 'POST',
                 url: url,
-                cache: false,
                 data: data,
                 dataType: 'jsonp',
                 success: function(res) {
                     for (var i=0, len=res[key].length;  i<len; i++) {
-                        var _item = '';
+                        var items = '';
                         var data = res[key];
                         var container = key.slice(11);
                         for (var i=0, len = data.length; i<len; i++) {
-                            _item += m.sprintf(item, data[i].listingUrl, data[i].imgUrl, data[i].listingName, data[i].minPrice, data[i].maxPrice)
+                            items += that.sprintf(that.item, data[i].listingUrl, data[i].imgUrl, data[i].listingName, data[i].minPrice, data[i].maxPrice)
                         }
                     }
-                    $('#'+container).append('<ul class="ul-stlye clearfix">'+ _item +'</ul></div>');
+                    $('#'+container).append('<ul class="ul-stlye clearfix">'+ items +'</ul></div>');
                 },
                 error: function(error){
                     console.log(error)
                 }
             })
-        },
-        stick: function(){
+        }
+        ,stick: function(){
+            var that = this;
             // navbar 滚动到顶部固定
             document.addEventListener('touchmove', function(event) {
-                var win_top = win.scrollTop();
-                var navAnchor_top = navAnchor.offset().top;
+                var win_top = $(window).scrollTop();
+                var navAnchor_top = that.navAnchor.offset().top;
                 if (win_top >= navAnchor_top) {
-                    navbar.addClass('stick');
-                    navAnchor.height(navbar.height());
+                    that.navbar.addClass('stick');
+                    that.navAnchor.height(that.navbar.height());
                 }
                 else {
-                    navbar.removeClass('stick');
-                    navAnchor.height(0);
+                    that.navbar.removeClass('stick');
+                    that.navAnchor.height(0);
                 }
             });
-            win.scroll(function(event) {
-                var win_top = win.scrollTop();
-                var navAnchor_top = navAnchor.offset().top;
+            $(window).scroll(function(event) {
+                var win_top = $(window).scrollTop();
+                var navAnchor_top = that.navAnchor.offset().top;
                 if (win_top >= navAnchor_top) {
-                    navbar.addClass('stick');
-                    navAnchor.height(navbar.height());
+                    that.navbar.addClass('stick');
+                    that.navAnchor.height(that.navbar.height());
                 }
                 else {
-                    navbar.removeClass('stick');
-                    navAnchor.height(0);
+                    that.navbar.removeClass('stick');
+                    that.navAnchor.height(0);
                 }
             })
-        },
-        nav: function(){
+        }
+        ,navTap: function(){
             var that = this;
-            navbar.find('li').click(function(){
+            that.navbar.find('li').click(function(){
                 var _this = $(this);
-                var box_top = box.eq(_this.index()).offset().top;
-                var navbar_h = navbar.height();
+                var box_top = that.box.eq(_this.index()).offset().top;
+                var navbar_h = that.navbar.height();
                 _this.addClass('active').siblings().removeClass('active');
                 if ($('html').scrollTop()) {  
                     $('html').scrollTop(box_top - navbar_h); 
@@ -108,16 +94,16 @@ $(function(){
                 else {
                     $('body').scrollTop(box_top - navbar_h);
                 }    
-                navbar.addClass('stick');
-                navAnchor.height(navbar.height()); 
+                that.navbar.addClass('stick');
+                that.navAnchor.height(that.navbar.height()); 
                 return false;  
             });
-            win.scroll(function(){
-                var win_top = win.scrollTop();
+            $(window).scroll(function(){
+                var win_top = $(window).scrollTop();
                 that.selectNav(win_top);
             })
-        },
-        navScroll:function(){
+        }
+        ,navScroll:function(){
             var myscroll;
             function loaded(){
                 setTimeout(function(){
@@ -125,10 +111,10 @@ $(function(){
                 },100 );
             }
             window.addEventListener("load",loaded,false);
-        },
-        selectNav: function(winTop){
-            var winH = win.height();
-            var navLi = navbar.find('li');
+        }
+        ,selectNav: function(winTop){
+            var winH = $(window).height();
+            var navLi = this.navbar.find('li');
             navLi.each(function(index){
                 if (index < navLi.length) {
                     var id = $(this).attr("data-id");
@@ -138,6 +124,14 @@ $(function(){
                     }
                 }
             });      
+        }
+        // 格式化字符串
+        ,sprintf: function(){
+            var str = arguments[0] || '';
+            for (var i=1, len=arguments.length; i<len; i++) {
+                str = str.replace(/%s/, arguments[i].toString());
+            }
+            return str;
         }
     }
 
